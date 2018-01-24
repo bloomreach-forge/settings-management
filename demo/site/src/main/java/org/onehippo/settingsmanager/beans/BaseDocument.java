eb250da3da651874671888d8b6f47dd0abf8edfc
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2018-01-24 14:30:15 +0100

    HIPFORGE-133 upgrading to 12: remove demo/*, demo to be rebuilt

--- demo/site/src/main/java/org/onehippo/settingsmanager/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.settingsmanager.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="settingsmanager:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
